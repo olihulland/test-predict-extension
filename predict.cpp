2f41fd53940f7928f4747a092977425a5f594ccc
Oliver Hulland <olihulland@gmail.com>
2023-05-31 23:43:57 +0100

    test dmesg and featurevector

--- predict.cpp
+++ predict.cpp
@@ -8,6 +8,7 @@ namespace predict {
     //%
     int predict(float * featureVector) {
         #if MICROBIT_CODAL
+            DMESGF("test %d , %d", (int)featureVector[0], (int)featureVector[1]);
             return uBit.timer.getTime();
         #else
             target_panic(PANIC_VARIANT_NOT_SUPPORTED);
