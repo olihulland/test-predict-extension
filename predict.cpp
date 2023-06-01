715f025c5c03dfe3cfa201a1056b425cf9550d77
Oliver Hulland <olihulland@gmail.com>
2023-06-01 02:24:33 +0100

    parse string to float arr

--- predict.cpp
+++ predict.cpp
@@ -18,7 +18,25 @@ namespace predict {
                 }
             }
 
-            return len;
+            float * data = (float *)malloc(sizeof(float) * len);
+            
+            // parse the string into the float array data
+            int index = 0;
+            int start = 0;
+            for (int i = 0; i < strLen; i++) {
+                if (strData[i] == ',') {
+                    int end = i - 1;
+                    int numLen = end - start + 1;
+                    char * numStr = (char *)malloc(sizeof(char) * (numLen + 1));
+                    memcpy(numStr, strData + start, numLen);
+                    numStr[numLen] = '\0';
+                    data[index] = atof(numStr);
+                    index++;
+                    start = i + 1;
+                }
+            }
+
+            return (int)data[0];
         #else
             target_panic(PANIC_VARIANT_NOT_SUPPORTED);
         #endif
