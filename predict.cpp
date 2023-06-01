70950ea737ee1e131d5ec95b5592f992cf8b9926
Oliver Hulland <olihulland@gmail.com>
2023-06-01 01:18:34 +0100

    typo for len

--- predict.cpp
+++ predict.cpp
@@ -9,7 +9,7 @@ namespace predict {
     int predict(String featureVector) {
         #if MICROBIT_CODAL           
             const char * strData = PXT_STRING_DATA(featureVector);
-            int len = PXT_STRING_LENGTH(featureVector);
+            int len = PXT_STRING_DATA_LENGTH(featureVector);
             return len;
         #else
             target_panic(PANIC_VARIANT_NOT_SUPPORTED);
